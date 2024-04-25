6976aa433522af3fe964a391ab02c6e7502188a7
Khushi <khushisinha280@gmail.com>
2024-04-25 10:28:38 +0530

    Add ecom api

---
+++ src/features/like/like.schema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+export const likeSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    likeable:{
+        type: mongoose.Schema.Types.ObjectId,
+        refPath:'on_model'
+    },
+    on_model:{
+        type:String,
+        enum:['Product','Category']
+    }
+}).pre('save',(next)=>{
+    console.log("Like is coming in");
+    next();
+}).post('save',(doc)=>{
+    console.log("Likes is saved");
+    console.log(doc);
+}).pre('find',(next)=>{
+    console.log("Retrieving Likes");
+    next();
+}).post('find',(doc)=>{
+    console.log("Found all likes");
+    console.log(doc);
+})
